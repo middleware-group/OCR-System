d99fe7ca87aed22e20d4c2e1754d840de97f8afa
YYhyang <44155580+YYhyang@users.noreply.github.com>
2019-05-30 00:31:12 +0800

    新增高精度与基础两个接口

--- OCR_YYH/src/main/java/com/midware/ocr/OCRController.java
+++ OCR_YYH/src/main/java/com/midware/ocr/OCRController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -30,8 +31,8 @@ public class OCRController {
     private static final String API_KEY="6n1vGtKLFSZBHzFIRb1kt1VT";
     private static final String SECRET_KEY="SogkfUiPefY0Prfrg0zq4nMjadkv5RtL";
 
-    @RequestMapping(value = "/ocr",method = RequestMethod.POST)
-    public Map<String, Object> ocr(@RequestParam("file") MultipartFile file) throws Exception{
+    @RequestMapping(value = "/basicOcr",method = RequestMethod.POST)
+    public Map<String, Object> basicOcr(@RequestParam("file") MultipartFile file) throws Exception{
 
         //OCRTX
         byte[] buf=file.getBytes();
@@ -39,6 +40,28 @@ public class OCRController {
         HashMap<String,String> options=new HashMap<String,String>();
         options.put("language_type","CHN_ENG");
         JSONObject res=client.basicGeneral(buf,options);
+        System.out.println(res.toString(2));
+
+        //保存文件
+        String path = System.getProperty("user.dir") + "\\src\\main\\resources\\images";
+        String name = file.getOriginalFilename();//直接返回文件的名字
+        String subffix = name.substring(name.lastIndexOf(".") + 1, name.length());//我这里取得文件后缀
+        String fileName = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        file.transferTo(new File(path+"\\" + fileName + "."+subffix));
+        String realpath = agreement + address + ":" + port + "/image" + "/" + fileName+"."+ subffix;
+        res.put("fileName",realpath);
+        return res.toMap();
+    }
+
+    @RequestMapping(value = "/accurateOcr",method = RequestMethod.POST)
+    public Map<String, Object> accurateOcr(@RequestParam("file") MultipartFile file) throws Exception{
+
+        //OCRTX
+        byte[] buf=file.getBytes();
+        AipOcr client=new AipOcr(APP_ID,API_KEY,SECRET_KEY);
+        HashMap<String,String> options=new HashMap<String,String>();
+        options.put("language_type","CHN_ENG");
+        JSONObject res=client.accurateGeneral(buf,options);
         System.out.println(res.toString(2));
 
         //保存文件
